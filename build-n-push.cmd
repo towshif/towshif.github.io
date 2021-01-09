06def6a029fb0e47095b18aeed0c5f0b4513ad9a
krazykoder <noddy4nods@gmail.com>
2021-01-09 12:46:56 -0800

    Build and committed $(date)

--- build-n-push.cmd
+++ build-n-push.cmd
@@ -7,7 +7,11 @@ rem # rsync to sync SRC and DEST --delete option to delete files in DEST which a
 rem # rsync /home/towshif/workspace/next/.docs/docs/ ./docs -ar --delete
 rem # cp ~/workspace/next/.docs/* . -r
 
-rem need mkdocs w/ python: pip install mkdocs 
+rem need mkdocs w/ python and material-theme: 
+rem pip install mkdocs 
+rem pip install mkdocs-material
+rem For more themes vitis: https://github.com/mkdocs/mkdocs/wiki/MkDocs-Themes
+rem just configure mkdocs.yml
 
 
 mkdocs build
